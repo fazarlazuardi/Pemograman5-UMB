f5e693d54765905bb56ca3cc5086a7af10d90453
ilham75257 <ilhamnurhikmat752@gmail.com>
2025-06-08 23:54:51 +0700

    pengiriman UAS

---
+++ Assets/_Complete-Game/Scripts/LevelTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waktu
+{
+    private float lamanya;
+}
+public class LevelTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float waktu = 90;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waktu > 0)
+        {
+            waktu -= Time.deltaTime;
+        }
+    }
+}
